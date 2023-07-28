c3850930ff3e6f5749326abfb878ea53bbf84d38
adecuellarv <76180286+adecuellarv@users.noreply.github.com>
2023-07-28 04:47:45 -0600

    delete services

--- src/services/messari.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-export const getAsset = async (name) => {
-    const infoAsset = await axios.get(`https://data.messari.io/api/v1/assets/${name}/metrics`);
-    if (infoAsset?.data) {
-        console.log(infoAsset?.data?.data);
-    }
-}
